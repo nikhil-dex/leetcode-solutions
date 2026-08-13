db520c3a59b125fcd83050fe4450b7f1e9fbd7ef
Nikhil <jincossec@gmail.com>
2026-08-13 11:35:28 +0530

    Time: 3 ms (95.66%), Space: 44.7 MB (74.93%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -4,15 +4,15 @@ class Solution {
         int len = s.length();
         for(int i=0;i<len;i++){
             char val = s.charAt(i);
-            if(!stk.isEmpty()){
+            if(stk.isEmpty()){
+
+                stk.push(val);
+            }else{
                 if((val=='B' && stk.peek()=='A') || (val=='D' && stk.peek()=='C')){
                     stk.pop();
                 }else{
                     stk.push(val);
                 }
-
-            }else{
-                stk.push(val);
             }
         }
 
